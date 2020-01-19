e05dfd7348707f85e093b2e9639a2058c774f79a
pylyp <pylyp.kutov@gmail.com>
2020-01-19 16:47:15 +0100

    Add a controller

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BethanysPieShop.Models;
 
 namespace BethanysPieShop
 {
@@ -14,6 +15,8 @@ namespace BethanysPieShop
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
             services.AddControllersWithViews();
         }
 
