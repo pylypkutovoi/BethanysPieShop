e8a50cf4724c94c501b4831723f29ca0cee51b87
pylyp <pylyp.kutov@gmail.com>
2020-01-22 19:37:58 +0100

    Add shopping cart

--- PieShop/Startup.cs
+++ PieShop/Startup.cs
@@ -29,6 +29,10 @@ namespace BethanysPieShop
 
             services.AddScoped<IPieRepository, PieRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ShoppingCart>(sp => ShoppingCart.GetCart(sp));
+
+            services.AddHttpContextAccessor();
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
@@ -40,6 +44,7 @@ namespace BethanysPieShop
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
